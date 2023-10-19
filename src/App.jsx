52c3a0acf23cd8e7444a6c60c2049ef7fb9980fc
juanka13v <juanka13v@users.noreply.github.com>
2023-10-18 21:16:52 -0300

    feat: Setup React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <h2>React native</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
