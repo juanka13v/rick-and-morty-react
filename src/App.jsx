1bb747883373587a9364694391995e75696b20c8
juanka13v <juanka13v@users.noreply.github.com>
2023-10-18 22:28:26 -0300

    feat: Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import LocationsPage from "./pages/LocationsPage/LocationsPage";
+import EpisodesPage from "./pages/EpisodesPage/EpisodesPage";
+import Header from "./Components/Header/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className="App-container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/episodes" element={<EpisodesPage />} />
+            <Route path="/locations" element={<LocationsPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </Router>
     </>
   );
