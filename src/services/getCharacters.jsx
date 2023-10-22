3987bd11c1cf84b16c28c332c5c5f54e14e76605
juanka13v <juanka13v@users.noreply.github.com>
2023-10-21 21:22:10 -0300

    feat(services): add getCharacters service

---
+++ src/services/getCharacters.jsx
@@ -0,0 +1,8 @@
+const getCharacters = async () => {
+  const response = await fetch("https://rickandmortyapi.com/api/character/");
+  const data = await response.json();
+
+  return data.results;
+};
+
+export default getCharacters;
