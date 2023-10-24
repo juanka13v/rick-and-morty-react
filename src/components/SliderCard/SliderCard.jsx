db9a56f9a0b7b3a8dcfa94426b329fdb8934b89d
juanka13v <juanka13v@users.noreply.github.com>
2023-10-24 18:46:05 -0300

    refactor(SliderCard): change all ui of SliderCard

--- src/components/SliderCard/SliderCard.jsx
+++ src/components/SliderCard/SliderCard.jsx
@@ -1,38 +1,25 @@
 import styles from "./SliderCard.module.css";
+import { Link } from "react-router-dom";
 
 const SliderCard = ({ character }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.header}>
-        <span className={`${styles[character.status.toLowerCase()]}`}>
-          {character.status}
-        </span>
-      </div>
+      <Link className={styles.link} to={`/character/${character.id}`}></Link>
+      <span
+        className={`${styles.status} ${styles[character.status.toLowerCase()]}`}
+      >
+        {character.status}
+      </span>
       <div className={styles.body}>
-        <img
-          src={character.image}
-          alt={character.name}
-          className={styles.image}
-          loading="lazy"
-        />
+        <img src={character.image} alt={character.name} />
       </div>
       <div className={styles.footer}>
-        <div className={styles.data}>
-          <span>Name:</span>
-          <span>{character.name} </span>
-        </div>
-        <div className={styles.data}>
-          <span>Gender:</span>
-          <span>{character.gender}</span>
-        </div>
-        <div className={styles.data}>
-          <span>Species:</span>
-          <span>{character.species}</span>
-        </div>
-        <div className={styles.data}>
-          <span>Origin:</span>
-          <span>{character.origin.name}</span>
-        </div>
+        <h2>
+          {character.name}
+        </h2>
+        <p>
+          {character.gender}
+        </p>
       </div>
     </div>
   );
