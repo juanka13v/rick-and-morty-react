47d3f109f4c33ab874714e86fc18e49e6a0e23a9
juanka13v <juanka13v@users.noreply.github.com>
2023-10-27 23:21:34 -0300

    feat(constants): add variables

---
+++ src/constants.jsx
@@ -0,0 +1,27 @@
+// constants.js
+export const DataStatus = ["Alive", "Dead", "Unknown"];
+export const DataSpecies = [
+  "Human",
+  "Alien",
+  "Robot",
+  "Animal",
+  "Flora",
+  "Voice",
+  "Humanoid",
+  "Eternal",
+  "Unknown",
+  "Parasite",
+  "Plant",
+  "Machine",
+  "Mythological",
+  "Ghost",
+  "Meta-human",
+  "Alien Plant",
+  "Insectoid",
+  "Alien Humanoid",
+  "Alien Animal",
+  "Alien Robot",
+  "Alien Mythological",
+  "Alien Ghost",
+];
+export const DataGender = ["Female", "Male", "Genderless", "unknown"];
