93ef42a808c717b11ef4e989bcaf7805836ea96e
juanka13v <juanka13v@users.noreply.github.com>
2023-10-30 19:46:25 -0300

    rafactor(SearchInput): add handleName

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -2,16 +2,17 @@ import styles from "./SearchInput.module.css";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 
-const SearchInput = ({ size }) => {
+const SearchInput = ({ size, name, onChange }) => {
   return (
     <div className={styles.container}>
       <input
         type="text"
         className={`${styles.input} ${styles[size]}`}
         placeholder="Search Character"
+        onChange={onChange}
       />
       <Button type="primary" size={size === "big" && "large"}>
-        <Link to="/search">Search</Link>
+        <Link to={`/search?name=${name}`}>Search</Link>
       </Button>
     </div>
   );
