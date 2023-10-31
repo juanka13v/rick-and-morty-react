f9d4cff63ec0101bc34f72643066c5982512a43e
juanka13v <juanka13v@users.noreply.github.com>
2023-10-31 16:02:06 -0300

    refactor(constants): change DataSpecies constants

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,27 +1,16 @@
-// constants.js
 export const DataStatus = ["Alive", "Dead", "Unknown"];
+
 export const DataSpecies = [
   "Human",
   "Alien",
-  "Robot",
-  "Animal",
-  "Flora",
-  "Voice",
   "Humanoid",
-  "Eternal",
-  "Unknown",
-  "Parasite",
-  "Plant",
-  "Machine",
-  "Mythological",
-  "Ghost",
-  "Meta-human",
-  "Alien Plant",
-  "Insectoid",
-  "Alien Humanoid",
-  "Alien Animal",
-  "Alien Robot",
-  "Alien Mythological",
-  "Alien Ghost",
+  "unknown",
+  "Poopybutthole",
+  "Mythological Creature",
+  "Animal",
+  "Robot",
+  "Cronenberg",
+  "Disease",
 ];
+
 export const DataGender = ["Female", "Male", "Genderless", "unknown"];
