167151068ff10820bb10a670b530461b7b36476b
juanka13v <juanka13v@users.noreply.github.com>
2023-10-31 16:42:14 -0300

    refactor(services): add filters getCharacters

--- src/services/getCharacters.jsx
+++ src/services/getCharacters.jsx
@@ -1,8 +1,27 @@
-const getCharacters = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/character/");
+const getCharacters = async (filters = {}) => {
+  const { name, gender, species, status, page } = filters;
+
+  const url = new URL(`https://rickandmortyapi.com/api/character/`);
+
+  if (name) url.searchParams.append("name", name);
+
+  if (gender) url.searchParams.append("gender", gender);
+
+  if (status) url.searchParams.append("status", status);
+
+  if (species) url.searchParams.append("species", species);
+
+  if (page) url.searchParams.append("page", page);
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    return [];
+  }
+
   const data = await response.json();
 
-  return data.results;
+  return data;
 };
 
 export default getCharacters;
