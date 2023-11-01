8ae1a987b7871b86dfcd4eb29ba9d797d489925f
juanka13v <juanka13v@users.noreply.github.com>
2023-11-01 16:42:22 -0300

    feat(Pagination): add component

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,54 @@
+import styles from "./Pagination.module.css";
+import generatePageNumbers from "../../helpers/generatePageNumbers";
+import scrollToTop from "../../helpers/scrollToTop";
+
+const Pagination = ({ info, currentPage, handlePage }) => {
+  const pages = generatePageNumbers(currentPage, info?.pages);
+
+  if (!currentPage || !info?.pages) {
+    return null;
+  }
+
+  return (
+    <div className={styles.pagination}>
+      {currentPage > 1 && (
+        <span
+          className={`${styles.btn} ${styles.item}`}
+          onClick={() => {
+            handlePage(currentPage - 1);
+            scrollToTop();
+          }}
+        >
+          <i class="bx bx-left-arrow-alt"></i>
+        </span>
+      )}
+
+      {pages.map((page) => (
+        <span
+          className={`${styles.item} ${page === currentPage && styles.active}`}
+          onClick={() => {
+            handlePage(page);
+            scrollToTop();
+          }}
+          key={page}
+        >
+          {page}
+        </span>
+      ))}
+
+      {currentPage < info?.pages && (
+        <span
+          className={`${styles.btn} ${styles.item}`}
+          onClick={() => {
+            handlePage(currentPage + 1);
+            scrollToTop();
+          }}
+        >
+          <i class="bx bx-right-arrow-alt"></i>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
