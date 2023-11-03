eb95bb6536f44ddc9e5fb76d5cee3188d2e02958
juanka13v <juanka13v@users.noreply.github.com>
2023-11-03 00:14:20 -0300

    refactor(constants): add DataEpisodes

--- src/constants.jsx
+++ src/constants.jsx
@@ -14,3 +14,32 @@ export const DataSpecies = [
 ];
 
 export const DataGender = ["Female", "Male", "Genderless", "unknown"];
+
+export const DataEpisodes = [
+  {
+    season: 1,
+    episodes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  },
+  {
+    season: 2,
+    episodes: [12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+  },
+  {
+    season: 3,
+    episodes: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+  },
+  {
+    season: 4,
+    episodes: [32, 33, 34, 35, 36, 37, 38, 39, 40, 41],
+  },
+
+  {
+    season: 5,
+    episodes: [42, 43, 44, 45, 46, 47, 48, 49, 50, 51],
+  },
+
+  {
+    season: 6,
+    episodes: [52, 53, 54, 55, 56, 57, 58, 59, 60, 61],
+  },
+];
