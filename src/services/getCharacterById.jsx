cc51f37fb39410b30fa901a8dc74a52a9fe5295c
juanka13v <juanka13v@users.noreply.github.com>
2023-11-03 23:58:47 -0300

    refactor(services): add handle errors in getCharacterById service

--- src/services/getCharacterById.jsx
+++ src/services/getCharacterById.jsx
@@ -2,6 +2,21 @@ const getCharacterById = async (id) => {
   const response = await fetch(
     `https://rickandmortyapi.com/api/character/${id}`
   );
+
+  if (response.status === 404) {
+    return {
+      error: true,
+      message: `No character found with id: ${id}`,
+    };
+  }
+
+  if (response.status === 500) {
+    return {
+      error: true,
+      message: "Server error, please try again later",
+    };
+  }
+
   const data = await response.json();
 
   return data;
