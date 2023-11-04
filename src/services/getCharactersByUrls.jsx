df7046e69803a928af44208041ee0b02f39c7180
juanka13v <juanka13v@users.noreply.github.com>
2023-11-04 00:40:17 -0300

    fix(services): fix bug

--- src/services/getCharactersByUrls.jsx
+++ src/services/getCharactersByUrls.jsx
@@ -1,14 +1,7 @@
 const getCharactersByUrls = async (urls) => {
-  try {
-    const responses = await Promise.all(urls.map((url) => fetch(url)));
-    const data = await Promise.all(
-      responses.map((response) => response.json())
-    );
-    return data;
-  } catch (error) {
-    console.error(`Error fetching characters: `, error);
-    return [];
-  }
+  const responses = await Promise.all(urls.map((url) => fetch(url)));
+  const data = await Promise.all(responses.map((response) => response.json()));
+  return data;
 };
 
 export default getCharactersByUrls;
