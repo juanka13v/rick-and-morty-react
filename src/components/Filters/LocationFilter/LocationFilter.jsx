3f0ca8502539c0dfd0cf2fbd8cb8fc97c3e3a67d
juanka13v <juanka13v@users.noreply.github.com>
2023-11-03 23:35:50 -0300

    feat(LocationFilter): add component

---
+++ src/components/Filters/LocationFilter/LocationFilter.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import styles from "./LocationFilter.module.css";
+import { DataLocations } from "../../../constants";
+import Accordion from "../../Accordion/Accordion";
+import Tag from "../../Tag/Tag";
+
+const LocationFilter = ({ handleLocationId, locationId }) => {
+  const [activeLocation, setActiveLocation] = useState(locationId);
+
+  const handleClick = (newLocation) => {
+    handleLocationId(newLocation);
+    setActiveLocation(newLocation);
+  };
+
+  useEffect(() => {
+    setActiveLocation(locationId);
+  }, [locationId]);
+
+  return (
+    <section className={styles.container}>
+      <Accordion open="true" title="Locations">
+        <div className={styles.accordion}>
+          {DataLocations.map((item) => (
+            <Tag
+              text={item.name}
+              key={`location_${item.id}`}
+              active={activeLocation === item.id}
+              onClick={() => handleClick(item.id)}
+            />
+          ))}
+        </div>
+      </Accordion>
+    </section>
+  );
+};
+
+export default LocationFilter;
