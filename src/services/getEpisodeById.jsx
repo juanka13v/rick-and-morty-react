e051440c8e549c07af01ea37d2142442d572b5cb
juanka13v <juanka13v@users.noreply.github.com>
2023-11-04 00:24:49 -0300

    refactor(services): add handle errors to getEpisodeById service

--- src/services/getEpisodeById.jsx
+++ src/services/getEpisodeById.jsx
@@ -1,5 +1,14 @@
 const getEpisodeById = async (id) => {
   const response = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
+
+  if (response.status === 404) {
+    return { error: true, message: `No found episode with Id: ${id}` };
+  }
+
+  if (response.status === 500) {
+    return { error: true, message: `Server error, try again later` };
+  }
+
   const data = await response.json();
 
   return data;
