50151944a248a7c1f9a508142b13773a315f7cf2
juanka13v <juanka13v@users.noreply.github.com>
2023-11-05 20:02:55 -0300

    refacter(Loader): add size and padding variables

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,11 @@
-import styles from "./Loader.module.css"
+import styles from "./Loader.module.css";
 
-const Loader = () => {
+const Loader = ({ size="100px", padding="100px" }) => {
   return (
-    <div className={styles.container}>
-        <i className='bx bx-loader bx-spin'></i>
+    <div className={styles.container} style={{ padding: padding }}>
+      <i className="bx bx-loader bx-spin" style={{ fontSize: size }}></i>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
